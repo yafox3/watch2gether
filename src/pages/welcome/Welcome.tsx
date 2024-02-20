eb2a7c1e98a079d4debcf9ec299ebc7285504df9
yafox3 <ruslan.kret@yandex.ru>
2024-02-20 15:19:15 +0300

    fix: text size and padding on phones

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -3,7 +3,7 @@ import { CreateRoom } from './components/CreateRoom'
 const Welcome = () => {
 	return (
 		<div className='py-6'>
-			<h1 className='text-center text-5xl mb-48'>
+			<h1 className='text-center text-4xl sm:text-5xl mb-28 sm:mb-48'>
 				Watch<span className='text-yellow-400'>2</span>gether
 			</h1>
 
--- src/pages/welcome/components/CreateRoom.tsx
+++ src/pages/welcome/components/CreateRoom.tsx
@@ -3,10 +3,10 @@ import { Button, Input, Label } from '@/ui'
 const CreateRoom = () => {
 	return (
 		<div className='max-w-96 mx-auto p-6 border border-zinc-800 rounded-lg'>
-			<h2 className='text-3xl text-center mb-6'>Create room</h2>
+			<h2 className='text-2xl sm:text-3xl text-center mb-6'>Create room</h2>
 
 			<form action='submit'>
-				<Label htmlFor='username' className='block mb-2'>
+				<Label htmlFor='username' className='block text-xs sm:text-base mb-2'>
 					Username
 				</Label>
 				<Input id='username' placeholder='Enter your username' className='mb-8' />
