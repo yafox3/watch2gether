f4d8684384b275a7c0fb15607558f5a086d34f96
yafox3 <ruslan.kret@yandex.ru>
2024-02-24 15:13:19 +0300

    feat: add users from the state to the row of users, update styles

--- src/modules/users-row/components/User.tsx
+++ src/modules/users-row/components/User.tsx
@@ -1,10 +1,23 @@
+import { IUser } from '@/models'
 import { FC } from 'react'
+import { LuCrown } from 'react-icons/lu'
 
-const User: FC<{ username: string }> = ({ username }) => {
+interface UserProps extends IUser {}
+
+const User: FC<UserProps> = ({ username, isOwner }) => {
 	return (
-		<div>
-			<img src={`https://api.dicebear.com/7.x/bottts/svg?seed=${username}`} alt='User avatar' className='mb-2 mx-auto w-24 md:w-32' />
-			<p className='text-center mx-auto text-sm font-bold dark:text-neutral-300 max-w-[128px] break-words line-clamp-1'>{username}</p>
+		<div className='relative rounded-lg w-full py-4 dark:bg-zinc-900 border dark:border-black/10 shadow-lg'>
+			<img
+				src={`https://api.dicebear.com/7.x/bottts/svg?seed=${username}`}
+				alt='User avatar'
+				className='mb-2 mx-auto w-24 md:w-28'
+			/>
+			{isOwner && (
+				<LuCrown className='absolute -top-3 -right-3 rotate-45 text-yellow-400 mx-auto text-2xl' />
+			)}
+			<p className='text-center mx-auto text-sm font-bold dark:text-neutral-300 max-w-[128px] break-words line-clamp-1'>
+				{username}
+			</p>
 		</div>
 	)
 }
--- src/modules/users-row/components/UsersRow.tsx
+++ src/modules/users-row/components/UsersRow.tsx
@@ -1,21 +1,15 @@
+import { useRoomStore } from '@/store/room'
 import { User } from './User'
 
 const UsersRow = () => {
-	const users = [
-		'user1',
-		'user2',
-		'user3',
-		'user4',
-		'user5',
-		'user6'
-	]
+	const users = useRoomStore(state => state.users)
 
 	return (
 		<>
 			<h3 className='text-xl lg:text-3xl dark:text-neutral-300 mb-1 lg:mb-3'>Users</h3>
-			<div className='w-full grid grid-rows-1 grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 px-6 py-5 gap-6 border dark:border-zinc-800 rounded-lg'>
+			<div className='w-full grid grid-cols-[repeat(auto-fill,_minmax(12rem,_1fr))] gap-5 border dark:border-zinc-800 rounded-lg'>
 				{users.map(user => (
-					<User key={user} username={user} />
+					<User key={user.username} {...user} />
 				))}
 			</div>
 		</>
