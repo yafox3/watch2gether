d116f501632c7d4ead755af73f6703fa71a2de4d
yafox3 <ruslan.kret@yandex.ru>
2024-02-25 14:25:29 +0300

    feat: change user avatar

--- src/modules/users-row/components/User.tsx
+++ src/modules/users-row/components/User.tsx
@@ -1,25 +1,25 @@
 import { IUser } from '@/models'
 import { FC } from 'react'
-import { LuCrown } from 'react-icons/lu'
+import { GiCrown } from 'react-icons/gi'
 
 interface UserProps extends IUser {}
 
 const User: FC<UserProps> = ({ username, isOwner }) => {
 	return (
-		<div className='relative rounded-lg w-full py-4 dark:bg-zinc-900 border dark:border-black/10 shadow-lg'>
+		<div className='relative rounded-lg w-full'>
 			<img
-				src={`https://api.dicebear.com/7.x/bottts/svg?seed=${username}`}
+				src={`https://api.dicebear.com/7.x/notionists-neutral/svg?seed=${username}&radius=12&backgroundColor=e5e5e5`}
 				alt='User avatar'
-				className='mb-2 mx-auto w-24 md:w-28'
+				className={`mb-2 mx-auto`}
 			/>
 			{isOwner && (
-				<LuCrown className='absolute -top-3 -right-3 rotate-45 text-yellow-400 mx-auto text-2xl' />
+				<GiCrown className='absolute -top-7 left-[55%] translate-x-[-50%] text-yellow-400 mx-auto text-4xl' />
 			)}
 			<p className='text-center mx-auto text-sm font-bold dark:text-neutral-300 max-w-[128px] break-words line-clamp-1'>
 				{username}
 			</p>
 		</div>
-	)
+	) //
 }
 
 export { User }
