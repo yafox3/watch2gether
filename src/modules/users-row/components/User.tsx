f9361e40fa044352a41eb63ce6cf6399a31eafc7
yafox3 <ruslan.kret@yandex.ru>
2024-02-26 11:13:20 +0300

    fix: users width

--- src/modules/users-row/components/User.tsx
+++ src/modules/users-row/components/User.tsx
@@ -6,11 +6,12 @@ interface UserProps extends IUser {}
 
 const User: FC<UserProps> = ({ username, isOwner }) => {
 	return (
-		<div className='relative rounded-lg w-full'>
+		<div className='relative rounded-lg w-[7rem]'>
 			<img
 				src={`https://api.dicebear.com/7.x/notionists-neutral/svg?seed=${username}&radius=12&backgroundColor=e5e5e5`}
 				alt='User avatar'
 				className={`mb-2 mx-auto`}
+				draggable={false}
 			/>
 			{isOwner && (
 				<GiCrown className='absolute -top-7 left-[55%] translate-x-[-50%] text-yellow-400 mx-auto text-4xl' />
--- src/modules/users-row/components/UsersRow.tsx
+++ src/modules/users-row/components/UsersRow.tsx
@@ -7,7 +7,7 @@ const UsersRow = () => {
 	return (
 		<>
 			<h3 className='text-xl lg:text-3xl dark:text-neutral-300 mb-1 lg:mb-3'>Users</h3>
-			<div className='w-full grid grid-cols-[repeat(auto-fill,_minmax(7rem,_1fr))] pb-4 pt-7 px-4 gap-6 border dark:border-zinc-800 rounded-lg'>
+			<div className='w-full grid grid-cols-[repeat(auto-fill,_minmax(7rem,_1fr))] place-items-center pb-4 pt-7 px-4 gap-6 border dark:border-zinc-800 rounded-lg'>
 				{users.map(user => (
 					<User key={user.username} {...user} />
 				))}
