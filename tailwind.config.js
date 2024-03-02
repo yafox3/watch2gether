90e8f8ca8ceacb8d55d95a315199b9cdc3355949
yafox3 <ruslan.kret@yandex.ru>
2024-03-02 08:34:03 +0300

    style: change the animation and position of the toast

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,18 @@ export default {
 					'0%': { transform: 'translateX(-100%)' },
 					'100%': { transform: 'translateX(0)' }
 				},
+				'slide-out-to-right': {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(100%)' }
+				},
+				'slide-in-from-bottom': {
+					'0%': { transform: 'translateY(100%)' },
+					'100%': { transform: 'translateY(0)' }
+				},
+				'slide-in-from-top': {
+					'0%': { transform: 'translateY(-100%)' },
+					'100%': { transform: 'translateY(0)' }
+				},
 				'in': {
 					'0%': { opacity: 0 },
 					'100%': { opacity: 1 }
@@ -35,6 +47,9 @@ export default {
 			animation: {
 				'slide-out-to-left': 'slide-out-to-left 0.3s ease-in-out',
 				'slide-in-from-left': 'slide-in-from-left 0.3s ease-in-out',
+				'slide-out-to-right': 'slide-out-to-right 0.2s ease-in-out',
+				'slide-in-from-bottom': 'slide-in-from-bottom 0.2s ease-in-out',
+				'slide-in-from-top': 'slide-in-from-top 0.2s ease-in-out',
 				'out': 'out 0.2s ease-in-out',
 				'in': 'in 0.2s ease-in-out',
 			},
