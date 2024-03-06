f82de74229c9178014975a6a31f6493ac372fb26
yafox3 <ruslan.kret@yandex.ru>
2024-03-06 09:13:52 +0300

    style: change display style

--- src/modules/room-header/components/DraggableVideo.tsx
+++ src/modules/room-header/components/DraggableVideo.tsx
@@ -16,7 +16,7 @@ const DraggableVideo = forwardRef(
 				<div className='relative h-full min-w-32 xs:min-w-40'>
 					<img src={img} alt='video preview' className='absolute -top-4 left-0 h-32 min-h-full' />
 				</div>
-				<div className='flex p-3'>
+				<div className='p-3'>
 					<h3 className='line-clamp-2 text-sm'>{title}</h3>
 					<p className='absolute bottom-1 right-2 text-xs sm:text-sm dark:text-zinc-400'>{index + 1}</p>
 				</div>
